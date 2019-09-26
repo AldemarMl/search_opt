5eabb75d212c0a115ffb69ec41a4a2c3d5c760ac
Víctor <victoribarbo@gmail.com>
2019-09-26 15:21:36 -0500

    Cambio título

--- search_opt/lib/main.dart
+++ search_opt/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Ronnin app'),
+      home: MyHomePage(title: 'Ronnin App'),
     );
   }
 }
